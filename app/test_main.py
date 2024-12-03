855cc2cc03e64a891d06932adec3297a1b470f5c
Serhiy <sergeynechaenko@ukr.net>
2024-12-03 11:10:45 +0200

    corrected side-effects

--- app/test_main.py
+++ app/test_main.py
@@ -4,11 +4,11 @@ from app.main import outdated_products
 
 
 def test_should_return_only_outdated_products() -> None:
-    orig_date = datetime.date
+    o_date = datetime.date
 
     with mock.patch("app.main.datetime.date") as mock_date:
-        mock_date.today.return_value = orig_date(2022, 2, 6)
-        mock_date.side_eff = lambda *args, **kwargs: orig_date(*args, **kwargs)
+        mock_date.today.return_value = o_date(2022, 2, 6)
+        mock_date.side_effect = lambda *args, **kwargs: o_date(*args, **kwargs)
 
         product_list = [
             {
@@ -35,11 +35,11 @@ def test_should_return_only_outdated_products() -> None:
 
 
 def test_should_return_empty_list_if_no_outdated_products() -> None:
-    orig_date = datetime.date
+    o_date = datetime.date
 
     with mock.patch("app.main.datetime.date") as mock_date:
-        mock_date.today.return_value = orig_date(2022, 2, 6)
-        mock_date.side_eff = lambda *args, **kwargs: orig_date(*args, **kwargs)
+        mock_date.today.return_value = o_date(2022, 2, 6)
+        mock_date.side_effect = lambda *args, **kwargs: o_date(*args, **kwargs)
 
         product_list = [
             {
